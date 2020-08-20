f9c1fcace7843257fc577ff47dc609760b43fd9f
justinmajetich <justinmajetich@gmail.com>
2020-08-20 14:32:09 -0400

    Initial commit

---
+++ 0x04-unity_publishing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 20, -10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
