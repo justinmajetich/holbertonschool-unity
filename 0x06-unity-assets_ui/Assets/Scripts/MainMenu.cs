ab2c26a2610be464b4ecee289e1b958c9a2e5d67
justinmajetich <justinmajetich@gmail.com>
2020-09-24 17:29:04 -0400

    Add Pause menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void Back()
+    {
+        // Load previous scene.
+        SceneManager.LoadScene(0);
+
+        // For dynamic back maybe...
+        // int previousScene = PlayerPrefs.GetInt("PreviousScene");
+        // SceneManager.LoadScene(previousScene);
+    }
+}
