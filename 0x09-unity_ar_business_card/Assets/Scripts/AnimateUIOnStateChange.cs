2a9ca7f1c216820ccef251d24d663742e6377c7e
justinmajetich <justinmajetich@gmail.com>
2020-11-04 10:37:25 -0500

    Implement UI animations

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/AnimateUIOnStateChange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateUIOnStateChange : MonoBehaviour
+{
+    private Animator animator;
+    private CanvasScaler canvasScaler;
+    private int introHash;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        canvasScaler = GetComponent<CanvasScaler>();
+    }
+
+    private void Start() {
+
+    }
+
+    public void PlayIntro() {
+        animator.SetBool("targetHasBeenFound", true);
+        canvasScaler.dynamicPixelsPerUnit += 1;
+        canvasScaler.dynamicPixelsPerUnit -= 1;
+    }
+
+    public void ExitIntroAnimation() {
+        animator.SetBool("targetHasBeenFound", false);
+    }
+}
---
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void Click() {
+        Debug.Log("Go to Twitter...");
+    }
+}
